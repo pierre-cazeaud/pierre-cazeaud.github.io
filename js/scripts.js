87b511334c6442663e20bff130e809819f5ded7c
pierre-cazeaud <pierre.cazeaud@gmail.com>
2016-04-11 09:23:02 +0200

    Index img : responsive + effets jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,23 @@ $(function(){
 
    $("#mobile").click(function(){
    		$("#menu ul").toggle();
-   })
+   });
 
 
-var img_work = $('#img-work');
-var title_work = $('#title-work');
+	$('.bkgd-work').css({'opacity':'0','margin-left':'-160px'});
+	$('.bkgd-work').delay(1000).animate({'opacity':'1','margin-left':'0'}, 1200);
+	$('#title-work').css({'opacity':'0', 'margin-top':'120px'});
+	$('#title-work').delay(1900).animate({'opacity':'1','margin-top':'100px'}, 900);
 
+	$('.bkgd-cv').css({'opacity':'0','margin-left':'-160'});
+	$('.bkgd-cv').delay(1300).animate({'opacity':'1','margin-left':'0'}, 1200);
+	$('#title-skills').css({'opacity':'0', 'margin-top':'120px'});
+	$('#title-skills').delay(2200).animate({'opacity':'1','margin-top':'100px'}, 900);
 
-	$(img_work).css({'opacity':'0','margin-left':'-800px'});
-	$(img_work).delay(1000).animate({'opacity':'1','margin-left':'-960px'}, 1200);
-	$(title_work).css({'opacity':'0', 'margin-top':'120px'});
-	$(title_work).delay(1900).animate({'opacity':'1','margin-top':'100px'}, 900);
-
-var img_cv = $('#img-skills');
-var title_cv = $('#title-skills');
-
-
-	$(img_cv).css({'opacity':'0','margin-left':'-1100px'});
-	$(img_cv).delay(1300).animate({'opacity':'1','margin-left':'-960px'}, 1200);
-	$(title_cv).css({'opacity':'0', 'margin-top':'120px'});
-	$(title_cv).delay(2200).animate({'opacity':'1','margin-top':'100px'}, 900);
-
-
-
-var img_contact = $('#img-contact');
-var title_contact = $('#title-contact');
-
-
-	$(img_contact).css({'opacity':'0','margin-left':'-800px'});
-	$(img_contact).delay(1600).animate({'opacity':'1','margin-left':'-960px'}, 1200);
-	$(title_contact).css({'opacity':'0', 'margin-top':'120px'});
-	$(title_contact).delay(2500).animate({'opacity':'1','margin-top':'100px'}, 900);
+	$('.bkgd-contact').css({'opacity':'0','margin-left':'-160'});
+	$('.bkgd-contact').delay(1600).animate({'opacity':'1','margin-left':'0'}, 1200);
+	$('#title-contact').css({'opacity':'0', 'margin-top':'120px'});
+	$('#title-contact').delay(2500).animate({'opacity':'1','margin-top':'100px'}, 900);
 
 
 /*var bloc = $('.bloc img');
